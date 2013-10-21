47187761fe82ab911346ed8c4998526854e848ba
Karissa McKelvey <krmckelv@gmail.com>
2013-10-21 15:01:43 -0500

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+COMMIT_HASH=`git show --quiet | head -n1`
+VERSION=`python -c "print '$COMMIT_HASH'.split()[-1][:8]"`
+BUCKET="wakari-static"
+APP_NAME="terminal"
+PATH_PREFIX="apps/$APP_NAME"
+
+s3cmd -c .s3cfg sync --recursive static/ s3://$BUCKET/$PATH_PREFIX/$VERSION/
+
+URL="https://d3uybqv7a64u59.cloudfront.net/$PATH_PREFIX/$VERSION"
+echo "CDN URL $URL"
+
+mkdir -p $HOME/.tty.js
+echo "{ \"cdn\" : \"$URL\" }" > $HOME/.tty.js/config.json
