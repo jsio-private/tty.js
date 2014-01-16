791ff27f306ceb19b177687bae716de7b60f8ac2
Bradley Kreider <bkreider@continuum.io>
2014-01-15 19:29:15 -0500

    fix shebang line

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -5,6 +5,14 @@ cd $SRC_DIR
 
 npm --loglevel warn install -g 
 
+# Make nodejs app environment-safe -- remove /usr/bin/env line
+pushd $PREFIX
+mv lib/node_modules/tty.js/bin/tty.js lib/node_modules/tty.js/bin/tty.js.old
+echo "#!${PREFIX}/bin/node" >  lib/node_modules/tty.js/bin/tty.js
+tail -n +2 lib/node_modules/tty.js/bin/tty.js.old >> lib/node_modules/tty.js/bin/tty.js
+popd
+
+
 POST_LINK=$PREFIX/bin/.${PKG_NAME}-post-link.sh
 cp $RECIPE_DIR/post-link.sh $POST_LINK
 chmod +x $POST_LINK
